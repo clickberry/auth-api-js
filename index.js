3eb022108aa028630fd622c0d1f79227ca579bab
Alexey Melnikov <alexey@Alexeys-Mac-mini.local>
2015-11-10 21:06:38 +0600

    Facebook authentication

--- index.js
+++ index.js
@@ -6,19 +6,40 @@
 
   var api = function (url) {
     return {
+      // Sign up by email and password
       signup: function (email, password, fn) {
         var data = {
           email: email,
           password: password
         };
 
-        $.post(url, data)
+        $.post(url + '/signup', data)
           .done(function(result) {
             fn(null, result);
           })
           .fail(function(jqXHR, textStatus, err) {
             fn(err);
           });
+      },
+
+      // Sets redirect url after social registrations
+      setRedirect: function (redirect_url) {
+        var data = {
+          callbackUri: redirect_url
+        };
+
+        $.post(url + '/social', data)
+          .done(function(result) {
+            fn(null, result);
+          })
+          .fail(function(jqXHR, textStatus, err) {
+            fn(err);
+          });
+      },
+
+      // Registration/login via Facebook
+      facebook: function () {
+        window.location.href = url + '/facebook';
       }
     };
   };
