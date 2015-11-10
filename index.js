cab36daa6a7b38623ff429f4ecdc164daafaee1d
Alexey Melnikov <alexey@Alexeys-Mac-mini.local>
2015-11-11 02:35:38 +0600

    Full Auth API

--- index.js
+++ index.js
@@ -30,7 +30,10 @@
           callbackUri: redirect_url
         };
 
-        $.post(url + '/social', data)
+        $.ajax({
+            url: url + '/social',
+            type: 'POST'
+          })
           .done(function(result) {
             fn(null, result);
           })
@@ -42,6 +45,135 @@
       // Registration/login via Facebook
       facebook: function () {
         window.location.href = url + '/facebook';
+      },
+
+      // Registration/login via Twitter
+      twitter: function () {
+        window.location.href = url + '/twitter';
+      },
+
+      // Registration/login via Google
+      twitter: function () {
+        window.location.href = url + '/google';
+      },
+
+      // Registration/login via Vk
+      vk: function () {
+        window.location.href = url + '/vk';
+      },
+
+      // Updates access & refresh tokens
+      refresh: function (refreshToken, fn) {
+        $.ajax({
+            url: url + '/refresh',
+            type: 'GET',
+            headers: {'Authorization': 'JWT ' + refreshToken}
+          })
+          .done(function(result) {
+            fn(null, result);
+          })
+          .fail(function(jqXHR, textStatus, err) {
+            fn(err);
+          });
+      },
+
+      // Signs out current user
+      signout: function (refreshToken, fn) {
+        $.ajax({
+            url: url + '/signout',
+            type: 'DELETE',
+            headers: {'Authorization': 'JWT ' + refreshToken}
+          })
+          .done(function() {
+            fn();
+          })
+          .fail(function(jqXHR, textStatus, err) {
+            fn(err);
+          });
+      },
+
+      // Deletes all sessions for current user
+      signoutall: function (refreshToken, fn) {
+        $.ajax({
+            url: url + '/signoutall',
+            type: 'DELETE',
+            headers: {'Authorization': 'JWT ' + refreshToken}
+          })
+          .done(function() {
+            fn();
+          })
+          .fail(function(jqXHR, textStatus, err) {
+            fn(err);
+          });
+      },
+
+      // Merges two accaunts
+      merge: function (access_token1, access_token2) {
+        var data = {
+          token1: access_token1,
+          token2: access_token2
+        };
+
+        $.ajax({
+            url: url + '/merge',
+            type: 'POST'
+          })
+          .done(function() {
+            fn();
+          })
+          .fail(function(jqXHR, textStatus, err) {
+            fn(err);
+          });
+      },
+
+      // Unmerges social account
+      unmerge: function (access_token, provider, id) {
+        var data = {
+          provider: provider,
+          id: id
+        };
+
+        $.ajax({
+            url: url + '/unmerge',
+            type: 'DELETE',
+            headers: {'Authorization': 'JWT ' + access_token}
+          })
+          .done(function() {
+            fn();
+          })
+          .fail(function(jqXHR, textStatus, err) {
+            fn(err);
+          });
+      },
+
+      // Gets user info
+      get: function (access_token) {
+        $.ajax({
+            url: url + '/account',
+            type: 'GET',
+            headers: {'Authorization': 'JWT ' + access_token}
+          })
+          .done(function(result) {
+            fn(null, result);
+          })
+          .fail(function(jqXHR, textStatus, err) {
+            fn(err);
+          });
+      },
+
+      // Deletes user account
+      del: function (access_token) {
+        $.ajax({
+            url: url + '/account',
+            type: 'DELETE',
+            headers: {'Authorization': 'JWT ' + access_token}
+          })
+          .done(function() {
+            fn();
+          })
+          .fail(function(jqXHR, textStatus, err) {
+            fn(err);
+          });
       }
     };
   };
